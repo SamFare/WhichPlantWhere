84659971fedad02881d3a596bda452d0b2d0a79e
SamFare <Samuel.fare@bglgroup.co.uk>
2018-06-19 21:04:01 +0100

    added finished UI layout

---
+++ app/src/test/java/samfare/me/whichplantwhere/PlantLoaderTests.java
@@ -0,0 +1,13 @@
+package samfare.me.whichplantwhere;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class PlantLoaderTests {
+    @Test
+    public void addition_isCorrect() {
+
+    }
+}
